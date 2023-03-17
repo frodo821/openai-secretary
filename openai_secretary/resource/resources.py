7116e0c41c24cecdc33919adcf6da240d788b711
frodo821 <sakaic2003@gmail.com>
2023-03-18 01:13:05 +0900

    fixed default initial context

--- openai_secretary/resource/resources.py
+++ openai_secretary/resource/resources.py
@@ -18,10 +18,13 @@ initial_messages: list[tuple[RoleType, str]] = [
 
 def create_initial_context(conv: Conversation | None) -> list[ContextItem]:
   context = []
-  context.append({'role': 'system', 'content': f'起動時刻は{datetime.now().strftime("%Y年%m月%d日 %H時%M分%S秒")}です'})
+  context.append({'role': 'system', 'content': f'あなたの今回の起動時刻は{datetime.now().strftime("%Y年%m月%d日 %H時%M分%S秒")}です'})
   if conv is not None:
-    context.append({'role': 'system', 'content': f'前回終了時刻は{conv.last_interact_at.strftime("%Y年%m月%d日 %H時%M分%S秒")}です。'})
+    context.append({
+      'role': 'system',
+      'content': f'あなたの前回終了時刻は{conv.last_interact_at.strftime("%Y年%m月%d日 %H時%M分%S秒")}です。'
+    })
   else:
-    context.append({'role': 'system', 'content': '前回終了時刻は記録されていません。今回が初めての起動です。'})
+    context.append({'role': 'system', 'content': '前回終了時刻は記録されていません。今回があなたの初めての起動です。'})
 
   return context
