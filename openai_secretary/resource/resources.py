2a28b2f82d04560271dd70ab227221e3d6c43504
frodo821 <sakaic2003@gmail.com>
2023-03-18 00:54:29 +0900

    fix initial prompt on start

--- openai_secretary/resource/resources.py
+++ openai_secretary/resource/resources.py
@@ -16,9 +16,12 @@ initial_messages: list[tuple[RoleType, str]] = [
 ]
 
 
-def create_initial_context(conv: Conversation) -> list[ContextItem]:
+def create_initial_context(conv: Conversation | None) -> list[ContextItem]:
   context = []
-  context.append({'role': 'system', 'content': f'起動時刻: {datetime.now().strftime("%Y年%m月%d日 %H時%M分%S秒")}'})
-  context.append({'role': 'system', 'content': f'前回終了時刻: {conv.last_interact_at.strftime("%Y年%m月%d日 %H時%M分%S秒")}'})
+  context.append({'role': 'system', 'content': f'起動時刻は{datetime.now().strftime("%Y年%m月%d日 %H時%M分%S秒")}です'})
+  if conv is not None:
+    context.append({'role': 'system', 'content': f'前回終了時刻は{conv.last_interact_at.strftime("%Y年%m月%d日 %H時%M分%S秒")}です。'})
+  else:
+    context.append({'role': 'system', 'content': '前回終了時刻は記録されていません。今回が初めての起動です。'})
 
   return context
