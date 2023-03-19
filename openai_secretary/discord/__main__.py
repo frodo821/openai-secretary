047daa459f14eead9ae5c9fa1d78a9cded621644
frodo821 <sakaic2003@gmail.com>
2023-03-19 14:51:25 +0900

    response ratio defaults to 0.9

--- openai_secretary/discord/__main__.py
+++ openai_secretary/discord/__main__.py
@@ -4,6 +4,6 @@ from openai_secretary.discord import OpenAIChatBot
 with open(join(dirname(__file__), '..', '..', '.discord.secret')) as f:
   key = f.read().strip()
 
-bot = OpenAIChatBot(key, response_ratio=0.4)
+bot = OpenAIChatBot(key, response_ratio=0.9)
 
 bot.start()
