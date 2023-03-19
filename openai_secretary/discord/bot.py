509947e2da6ff1f02f5e704f6e95d5b7f4f5792f
frodo821 <sakaic2003@gmail.com>
2023-03-20 08:38:23 +0900

    refactored command processing into a separate method

--- openai_secretary/discord/bot.py
+++ openai_secretary/discord/bot.py
@@ -91,6 +91,20 @@ class OpenAIChatBot:
       case ['emotion']:
         await message.channel.send(f'`[SYSTEM]` 現在の感情は{repr(self.agents[cid].emotion)}です。')
 
+  async def process_command(self, message: Message) -> None:
+    cmd, *args = message.content.strip().split(None, 1)
+    name = f'cmd_{cmd[len(self.cmd_prefixes[message.channel.id]):].replace("-", "_")}'
+    if hasattr(self, name):
+      await getattr(self, name)(message, (args or [''])[0])
+    else:
+      await message.channel.send(f'`[SYSTEM]` コマンド「{cmd}」は存在しません。')
+    return
+  
+  def is_mentioned(self, message: Message) -> bool:
+    return self.client.user in message.mentions or any(
+      any(self.client.user == mem._user for mem in role.members) for role in message.role_mentions
+    )
+
   async def on_message(self, message: Message) -> None:
     cid = message.channel.id
 
@@ -105,14 +119,9 @@ class OpenAIChatBot:
     if message.mentions and self.client.user not in message.mentions:
       return
 
+    # メッセージがコマンドプレフィクスから始まる場合はコマンドとして処理
     if message.content.startswith(self.cmd_prefixes[cid]):
-      cmd, *args = message.content.strip().split(None, 1)
-      name = f'cmd_{cmd[len(self.cmd_prefixes[cid]):].replace("-", "_")}'
-      if hasattr(self, name):
-        await getattr(self, name)(message, (args or [''])[0])
-      else:
-        await message.channel.send(f'`[SYSTEM]` コマンド「{cmd}」は存在しません。')
-      return
+      return await self.process_command(message)
 
     mentioned = self.client.user in message.mentions or any(
       any(self.client.user == mem._user for mem in role.members) for role in message.role_mentions
